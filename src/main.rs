e4c79b90588ed4f63dd43dac1ffa92aefe9d34b3
tieje <toj320@gmail.com>
2023-02-16 22:48:03 -0500

    work in progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Result, stdin};
+
+static FIRST_STRING: &str = "Would you like to create a file or folder?\nfile: press Enter\nfolder: type \"f\", then Enter";
+
+
+fn main() -> Result<()>{
+    println!("{}", FIRST_STRING);
+    let mut buffer = String::new();
+    let stdin = stdin();
+    stdin.read_line(&mut buffer)?;
+    let user_input: u32 = buffer.chars().nth(0).unwrap() as u32;
+    // TODO: split into separate functions and create tests for each function
+    println!("{}", user_input);
+    // match buffer {
+
+    // }
+    Ok(())
 }
