28c68b6b08010f6382322932c45c32f9124b4b2c
tieje <toj320@gmail.com>
2023-02-18 20:13:29 -0500

    final commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# file creator
+
+Basically, the same functionality as the `touch` unix command, except with prompts.
+
+I have decided to not build this out because its just not very interesting to me.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::{Result, stdin};
 
-static FIRST_STRING: &str = "Would you like to create a file or folder?\nfile: press Enter\nfolder: type \"f\", then Enter";
+static FIRST_STRING: &str = "\nWould you like to create a file or folder?\n\nfile: press Enter\n\nfolder: type \"f\", then Enter";
 
 
 fn main() -> Result<()>{
@@ -11,8 +11,12 @@ fn main() -> Result<()>{
     let user_input: u32 = buffer.chars().nth(0).unwrap() as u32;
     // TODO: split into separate functions and create tests for each function
     println!("{}", user_input);
-    // match buffer {
-
-    // }
+    // 10 is Enter, 101 is e, 102 is f
+    match user_input {
+        10 => println!("Enter was entered, create a file"),
+        101 => println!("e was entered, end program"),
+        102 => println!("f was entered, create a folder"),
+        _ => println!("Neither f nor Enter was entered")
+    }   
     Ok(())
 }
